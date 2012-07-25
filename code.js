324b0dfa3bbe807cfc61472fc3d62a7f4d707b09
Vadim Graboys <vadim@chartbeat.com>
2012-07-25 11:21:05 -0400

    shit's pretty dope

--- code.js
+++ code.js
@@ -1,5 +1,42 @@
 (function() {
 
+function render() {
+    $('.wt-partition').each(function(_, obj) {
+        var $obj = $(obj);
+        var th = $obj.height();
+        var tw = $obj.width();
+        var $childs = $(obj).children();
 
+        // remove stray spaces cuz they will fuck up the layout
+        $childs.remove();
+        $obj.text('');
+        $obj.append($childs);
+
+        if($obj.hasClass('wt-vertical')) {
+            $childs.width(tw);
+            $childs.each(function(i, child) {
+                var $child = $(child);
+
+                var height = Math.round(th / ($childs.length - i));
+                th -= height;
+
+                $child.height(height);
+            });
+        } else if($obj.hasClass('wt-horizontal')) {
+            $childs.height(th);
+            $childs.each(function(i, child) {
+                var $child = $(child);
+
+                var width = Math.round(tw / ($childs.length - i));
+                tw -= width;
+
+                $child.width(width);
+            });
+        }
+    });
+}
+
+render();
+$('.webtile').css('visibility', 'visible');
 
 })();
